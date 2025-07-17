923992a947787ae70bafe430da113f21c99c8233
taha588 <mohamedtahakchmar@gmail.com>
2025-07-17 16:13:36 +0100

    Delete op directory

--- op/services/geminiService.ts
+++
@@ -1,7 +0,0 @@
-import { GoogleGenAI } from "@google/genai";
-
-if (!process.env.API_KEY) {
-  throw new Error("La variable d'environnement API_KEY n'est pas définie.");
-}
-
-export const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
